68d8180589a1807f109eaaeacf28a5b27dfc102d
yoonbigbear <yoonbigbear@hotmail.com>
2024-08-29 01:37:31 +0900

    폴더 구조 변경 및 main.rs 코드 비움

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,5 @@ use std::sync::atomic::Ordering::Relaxed;
 use std::time::Duration;
 
 fn main() {
-    let mut non_static_value = 42;
-    std::thread::scope(|s| {
-        for i in 0..10 {
-            s.spawn(|| {
-                for i in 0..10 {
-                    non_static_value += i;
-                    println!("{}", non_static_value);
-                    std::thread::sleep(Duration::from_millis(10));
-                }
-            });
-        }
-    });
+    
 }
